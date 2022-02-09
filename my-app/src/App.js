b95740067f984796e5ae3e2d66ece9505bda9a41
bitf12m033 <umermj2@gmail.com>
2022-02-09 13:13:49 +0500

    Update App.js
    New file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Hello from './Hello';
 function App({name , con, age}) {
     return (
-      <div>Hello From {name} , from {con} , age {age}</div>
+      <div>
+        <p>
+
+        Hello From {name} , from {con} , age {age}
+        </p>
+        <Hello/>
+      </div>
     );
 }
 
