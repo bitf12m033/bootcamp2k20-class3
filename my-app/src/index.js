3f083d67cc38ace688f2f9c22f418f24ce3b11cf
bitf12m033 <umermj2@gmail.com>
2022-02-09 12:45:53 +0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <span> Umer </span>,
+ReactDOM.render( <App name="Umer"/>,
     document.getElementById('root')
 );
 
