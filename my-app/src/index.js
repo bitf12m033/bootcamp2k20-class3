368b4bbb66ed7f62ca78c243ba031c4b7b5cc4cf
bitf12m033 <umermj2@gmail.com>
2022-02-09 12:59:47 +0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <App name="Umer"/>,
+ReactDOM.render( <App name="Umer" con="Pakistan" age={25-10}/>,
     document.getElementById('root')
 );
 
